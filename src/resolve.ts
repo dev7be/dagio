9bb049481b33ed4e329f4b8a2476550d9f5b50f4
Antoine Marot <antmarot@gmail.com>
2023-03-09 13:12:03 +0100

    refactor: remove spread and immutability

--- src/resolve.ts
+++ src/resolve.ts
@@ -5,10 +5,10 @@ export type Resolved<T extends AnyDepsDict> = Expand<{
 }>;
 
 export const resolve = <T extends AnyDepsDict>(deps: T): Resolved<T> =>
-  Object.keys(deps).reduce(
-    (acc, c) => ({ ...acc, [c]: deps[c]() }),
-    {} as Resolved<T>,
-  );
+  Object.keys(deps).reduce((acc, c) => {
+    acc[c] = deps[c]();
+    return acc;
+  }, {} as any) as Resolved<T>;
 
 export const resolved =
   <T extends AnyDepsDict, U>(fn: (deps: Resolved<T>) => U) =>
