89b97b9f56bae42ed0f06467b45a04b0abab280b
Antoine Marot <antmarot@gmail.com>
2023-03-09 11:28:15 +0100

    refactor: unwind `define`

--- src/define.ts
+++ src/define.ts
@@ -1,17 +1,22 @@
-import type { AnyGraph, CommitStep } from './types';
+import type { AnyGraph } from './types';
 
-export type AddStep<G extends AnyGraph> = {
+export type Define<G extends AnyGraph> = {
   add: <K extends string, Needs extends ReadonlyArray<keyof G & string>>(
     k: Exclude<K, keyof G & string>,
     ...needs: Needs
-  ) => DefineStep<G & Readonly<{ [k in K]: Needs }>>;
+  ) => Define<G & Readonly<{ [k in K]: Needs }>>;
+
+  commit: () => { [k in keyof G]: G[k] };
 };
 
-export type DefineStep<G extends AnyGraph> = CommitStep<G> & AddStep<G>;
-
-const defineStep = <G extends AnyGraph>(graph: G): DefineStep<G> => ({
-  add: (k, ...needs) => defineStep({ ...graph, [k]: needs }),
-  commit: () => graph,
-});
-
-export const define = () => defineStep<Record<never, never>>({});
+export const define = () => {
+  const graph: Record<string, string[]> = {};
+  const step = {
+    add: (k: string, ...deps: string[]) => {
+      graph[k] = deps;
+      return step;
+    },
+    commit: () => graph,
+  };
+  return step as Define<Record<never, never>>;
+};
